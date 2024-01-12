09ceaf53fd984619d45d55da6349d96281e2d7b8
marialegl <psico.marialegl@gmail.com>
2024-01-12 13:58:02 -0800

    Correction for the file cleanMemory

--- cleanMemory.c
+++ cleanMemory.c
@@ -1,12 +1,18 @@
 #include "header.h"
 
+/**
+ * cleanMemory - Frees dynamically allocated memory
+ * for an input buffer and a word list.
+ * @buffer: Pointer to the dynamically allocated input buffer.
+ * @wordList: Pointer to the dynamically allocated array of words.
+ */
 void cleanMemory(char *buffer, char **wordList)
 {
 	int i;
 
 	free(buffer);
 
-        for (i = 0; wordList[i] != NULL; i++)
+	for (i = 0; wordList[i] != NULL; i++)
 	{
 		free(wordList[i]);
 	}
